ea3560f7fc368aae8f6cff34bb2b7127924fc789
robertotics4 <robertooliveiragp@gmail.com>
2023-11-12 11:51:22 -0300

    test: create new test in path to get formatted path

--- tests/domain/entities/Path.spec.ts
+++ tests/domain/entities/Path.spec.ts
@@ -17,4 +17,36 @@ describe('Path entity', () => {
 
     expect(path).toEqual(pathData);
   });
+
+  it('should return formatted path', () => {
+    const fakePath: Path = new Path({
+      routes: [
+        {
+          origin: 'GRU',
+          destination: 'BRC',
+          price: 10,
+        },
+        {
+          origin: 'BRC',
+          destination: 'SCL',
+          price: 5,
+        },
+        {
+          origin: 'SCL',
+          destination: 'ORL',
+          price: 20,
+        },
+        {
+          origin: 'ORL',
+          destination: 'CDG',
+          price: 5,
+        },
+      ],
+      price: 40,
+    });
+
+    const result = Path.getFormattedPath(fakePath);
+
+    expect(result).toEqual('GRU - BRC - SCL - ORL - CDG > $40');
+  });
 });
